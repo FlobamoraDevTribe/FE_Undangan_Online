a8cbecc5156f6d0ac680c8c44df9deb2c2d58e9e
RioBoy <boyrio68@gmail.com>
2024-01-13 19:41:50 +0800

    edit readme, install sass, tailwind and setting prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   return (
+      <div className="container px-[150px]">
+         <h5 className="text-lg text-teal-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
+            omnis reiciendis. Eveniet repellendus illum tempore deserunt omnis
+            quidem natus aliquam nesciunt suscipit odit quos quaerat facilis
+            commodi vel, error temporibus animi nulla ex a reprehenderit rem
+            similique dicta nemo unde. Tenetur ab vero nesciunt maiores eum
+            vitae totam quis nulla?
+         </h5>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   )
 }
 
 export default App
